f6a073bf29f0b6327587b099e2b70656b1c8a060
sylvpan <sylvpan@gmail.com>
2025-09-07 09:27:01 +0800

    普通提交

--- python/scrapyTstPrj001/anjuke/anjuke/spiders/spdrAnjuke.py
+++ python/scrapyTstPrj001/anjuke/anjuke/spiders/spdrAnjuke.py
@@ -10,6 +10,15 @@ class SpdranjukeSpider(scrapy.Spider):
         print(response.text)
         fangList = response.xpath('//ul[@class="list"]/li')
         for fang in fangList:
-            fangTitle = fang.xpath('./a/div[@class="content-wrap"]/div[@class="title-wrap lines2"]/span/text()').extract_first()
+            fangTitle = fang.xpath('./a/div[@class="content-wrap"]/div[@class="title-wrap lines2"]/span[@class="content-title"]/text()').extract_first()
+            fangHuxing = fang.xpath('./a/div[@class="content-wrap"]/div[@class="desc-wrap-community"]/span[@class="content-desc"]/text()').extract_first()
+            fangSize = ""
+            try:
+                fangSize = fang.xpath('./a/div[@class="content-wrap"]/div[@class="desc-wrap-community"]/span[@class="content-desc"]/text()')[1].extract()
+            except Exception as e:
+                pass
+            #fangHuxing = fang.xpath('./a/div[@class="content-wrap"]/div[@class="desc-wrap-community"]/span[@class="content-desc"]/text()').extract_first()
             print(fangTitle)
+            print(fangHuxing)
+            print(fangSize)
         pass
